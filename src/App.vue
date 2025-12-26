6b1a3e6e7d96c515e9877acb90192f402b78bdbe
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-12-26 23:55:10 +0600

    My new vue project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<p>My name is :{{name}}</p>
+<br>
+<p>My age is :{{age}}</p>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      name : "My name is sajjad Hossain JIM",
+      age : 24
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
